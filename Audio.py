6b639298818d0a24cc3dee543064c1b03029ed14
anmol pandey <anmol pandey>
2017-02-24 18:23:22 +0530

    Added a seperate FeatureExtraction Function

--- Audio.py
+++ Audio.py
@@ -2,13 +2,13 @@ import AppConfig
 import numpy
 from scipy.io import wavfile
 from pyAudioAnalysis import audioFeatureExtraction
-
 class Audio:
     def __init__(self, path):
         self.path = path
         self.singleFrame = []
         self.allFrames = []
         (self.fs, self.signal) = wavfile.read(path)
+        self.noFrames=len(self.signal)
 
     def getFeatureVector(self):
         featureVector = audioFeatureExtraction.stFeatureExtraction(self.signal, self.fs, AppConfig.getWindowSize(),
@@ -19,3 +19,22 @@ class Audio:
                 self.singleFrame.append(frames[requiredFeature])
             self.allFrames.append(self.singleFrame)
         return numpy.array(self.allFrames)
+    def getFeatureVector2(self):
+        featureVector=audioFeatureExtraction.stFeatureExtraction(self.signal,self.fs,AppConfig.getWindowSize(),AppConfig.getWindowHop())
+        filteredFeatureVector=[]
+        for i,feature in enumerate(featureVector):
+            if i>=8 and i<=20:
+                filteredFeatureVector.append(feature)
+        self.featureVector=filteredFeatureVector
+        print type(featureVector)
+        return numpy.transpose(filteredFeatureVector)
+    def getNoOfFrames(self):
+        return self.noFrames
+"""X=Audio(AppConfig.getFilePathTraining("en",34))
+print X
+FV=X.getFeatureVector()
+print FV
+print FV.shape
+FV2=X.getFeatureVector2()
+print FV2
+print FV2.shape"""
