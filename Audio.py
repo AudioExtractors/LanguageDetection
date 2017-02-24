439bcf4f2feebf77b63832050da04e098c9e9692
unknown <anmolvarshney05>
2017-02-24 16:16:10 +0530

    Modified to extract only required features

--- Audio.py
+++ Audio.py
@@ -1,10 +1,22 @@
 import AppConfig
+import numpy
 from scipy.io import wavfile
 from pyAudioAnalysis import audioFeatureExtraction
+
+
 class Audio:
-    def __init__(self,path):
-        self.path=path
-        (self.fs,self.signal)=wavfile.read(path)
-    def populateFeatureVector(self):
-        featureVector=audioFeatureExtraction.stFeatureExtraction(self.signal,self.fs,AppConfig.getWindowSize(),AppConfig.getWindownHop())
+    def __init__(self, path):
+        self.path = path
+        self.singleFrame = []
+        self.allFrames = []
+        (self.fs, self.signal) = wavfile.read(path)
+
     def getFeatureVector(self):
+        featureVector = audioFeatureExtraction.stFeatureExtraction(self.signal, self.fs, AppConfig.getWindowSize(),
+                                                                   AppConfig.getWindowHop())
+        for frames in featureVector:
+            self.singleFrame = []
+            for requiredFeature in AppConfig.featureNumbers:
+                self.singleFrame.append(frames[requiredFeature])
+            self.allFrames.append(self.singleFrame)
+        return numpy.array(self.allFrames)
