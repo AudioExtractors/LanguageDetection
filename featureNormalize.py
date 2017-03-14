3ff22341907d6834edfbe9bd8917346535ec5250
adityajain95 <adityajain95@yahoo.in>
2017-03-14 20:52:37 +0530

    Add files via upload
    Implemented feature normalisation

---
+++ featureNormalize.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#Normalises training data for better training
+def normalise(X):
+    mu = np.mean(X,axis=0)
+    sigma = np.std(X,axis=0,ddof=1)
+    X_norm = (X-mu)/sigma
+    return X_norm, mu, sigma
+
+#Converts test data to a format on which NN was trained
+def normConv(X,mu,sigma):
+    return (X-mu)/sigma
+
+#Tester
+if __name__ == "__main__":
+    a=np.array([[1,2],[3,4]])
+    print a
+    b,mu,sigma = normalise(a)
+    print b,mu,sigma
+    print normConv(a,mu,sigma)
