b63f816f7942268c8aa200a6dcbfa8a117ed2a57
adityajain95 <adityajain95@yahoo.in>
2017-04-03 16:37:04 +0530

    Feature normalisation with batch update

---
+++ feature_normalize.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# Normalises training data for better training
+class FeatureNormalise:
+    """Normalises data using mean std normalisation
+    Usage:
+        Call function batchData to train on data.
+        After whole data is received call fit and transform
+    """
+    def __init__(self, n_features):
+        self.count_ = 0
+        self.x = np.zeros(n_features, dtype=np.float64)
+        self.x2 = np.zeros(n_features, dtype=np.float64)
+        self.mu = 0
+        self.sigma = 1
+
+    def batchData(self, X):
+        self.count_ = self.count_ + X.shape[0]
+        self.x = self.x + np.sum(X, axis=0)
+        self.x2 = self.x2 + np.sum(X*X, axis=0)
+
+    def fit(self):
+        self.mu = self.x/self.count_
+        self.sigma = np.sqrt((self.x2 - 2*self.mu*self.x + self.count_*self.mu*self.mu)/(self.count_-1) )
+
+    def transform(self, X):
+        return (X-self.mu)/self.sigma
+
+
+# Tester
+if __name__ == "__main__":
+    a = np.array([[1, 2], [3, 4]])
+    print a
+    sel = FeatureNormalise(2)
+    sel.batchData(a)
+    sel.fit()
+    print sel.transform(a)
